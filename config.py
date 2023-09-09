fc5c6312791a219b0845e8ad42bc75a7afbc2216
fonnto <fonnto56@gmail.com>
2023-09-09 12:18:08 +0300

    Update v 0.2 alpha!

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 login=""
 password=""
+link=""
--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 import keyboard
-from config import login, password
+from config import login, password, link
 print("\n███████╗███╗░░░███╗██╗░░░░░"
          "\n██╔════╝████╗░█l███║██║░░░░░"
          "\n█████╗░░██╔████╔██║██║░░░░░"
          "\n██╔══╝░░██║╚██╔╝██║██║░░░░░"
          "\n██║░░░░░██║░╚═╝░██║███████╗"
          "\n╚═╝░░░░░╚═╝░░░░░╚═╝╚══════╝"
-         "\nauth to ts3.lu by fonnto v 0.1(pre alpha version)"
+         "\nautoauth by fonnto v 0.2(alpha version)"
          )
          
-sett=str(input("На какую кнопку должен вводиться логин? "))
-sett2=str(input("На какую кнопку должен вводиться пароль? "))
+key=str(input("На какую кнопку должен вводиться логин? "))
+key2=str(input("На какую кнопку должен вводиться пароль? "))
+key3=str(input("На какую кнопку введеться ссылка?(Если не нужно оставь пустую строку)"))
 def auth_log():
     for i in range(1):
-        keyboard.wait(sett)
+        keyboard.wait(key)
         keyboard.send("backspace")
         keyboard.write(login)
         i=1
 def auth_pass():
     for i in range(1):
-        keyboard.wait(sett2)
+        keyboard.wait(key2)
         keyboard.send("backspace")
         keyboard.write(password)
-       
-print("Скрипт работает! Нажми на клавиши которые выбрал!")
-auth_log()
-auth_pass()
+def auth_link():
+    for i in range(1):
+        keyboard.wait(key3)
+        keyboard.send("backspace")
+        keyboard.write(link)
+if key=="":
+    print("Ошибка кнопка ввода логина не назначена!")
+    eror=input("Перезапустите скрипт")
+if key2=="":
+    print("Ошибка кнопка ввода пароля не назначена!")
+    eror=input("Перезапустите скрипт")
+if key==key2:
+    print("Ошибка назначеные кнопки совпадают!")
+    eror=input("Перезапустите скрипт")
+if key3=="":
+    auth_log()
+    auth_pass()
+else:
+    auth_link()
+    auth_log()
+    auth_pass()
